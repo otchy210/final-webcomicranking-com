c5befeddfd9b0ddf5989509fad627c87011e31eb
Otchy <id+github@indeed.com>
2019-05-03 16:13:38 +0900

    0.0.3: Add redirect support

--- app.js
+++ app.js
@@ -2,17 +2,32 @@ const express = require('express');
 
 const app = express();
 
+// app config
+app.disable('x-powered-by');
+
+// static files
+app.use(express.static('public', {
+    index: false,
+    redirect: false
+}));
+
+// root
 app.get('/', (req, res) => {
+    if (req.hostname !== 'localhost' && req.get('X-Forwarded-Proto') === 'http') {
+        res.redirect(301, `https://${req.hostname}/`).end();
+        return;
+    }
     res
         .status(200)
         .send('WCR の跡地')
         .end();
 });
 
-app.use(express.static('public', {
-    index: false,
-    redirect: false
-}));
+// other docs
+const redirectToRoot = (req, res) => res.redirect(301, '/');
+const anyDocPaths = /.+(\/|\.html|\.php|\.cgi)$/;
+app.get(anyDocPaths, redirectToRoot);
+app.post(anyDocPaths, redirectToRoot);
 
 const listener = app.listen(8080, () => {
     const port = listener.address().port;
